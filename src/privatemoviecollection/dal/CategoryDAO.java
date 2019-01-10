a39152eb41a13e2eba6ef17ffaf5464b217e7d6c
PLKING <sz.soczewka@gmail.com>
2019-01-10 11:17:33 +0100

    Hello Mate

--- src/privatemoviecollection/dal/CategoryDAO.java
+++ src/privatemoviecollection/dal/CategoryDAO.java
@@ -2,12 +2,14 @@ package privatemoviecollection.dal;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import privatemoviecollection.be.Category;
+import privatemoviecollection.be.Movie;
 
 public class CategoryDAO
 {
@@ -47,5 +49,30 @@ public class CategoryDAO
         }
         return categories;
     }
+    
+    public void saveMovies() throws SQLException {
+        int ArraySize = listCategories.size();
+        try (Connection con = cp.getConnection()) {
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery("DELETE FROM Movies");
+            for (int i = 0; i < ArraySize; i++) {
+                Category actualCategory = listCategories.get(i);
+                String sql = "INSERT INTO Movies VALUES(?,?,?,?,?)";
+                PreparedStatement ppst = con.prepareStatement(sql);
+                ppst.setInt(1, actualCategory.getId());
+                ppst.setString(2, actualCategory.getName());
+                ppst.execute();
+            }
+        }
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     }
 
